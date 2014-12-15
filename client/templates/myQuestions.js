78a8784fd1b86dcaa83b0bc4831ae9dc34cda506
Max Shen <gshen@college.harvard.edu>
2014-12-15 15:54:23 -0500

    debug

--- client/templates/myQuestions.js
+++ client/templates/myQuestions.js
@@ -1,4 +1,4 @@
-Template.questionsPage.helpers({
+Template.myQuestions.helpers({
     // returns all questions in the DB
     questions: function () {
       return Questions.find({userId: Meteor.userId()}, {sort: {createdAt: -1}});
