e0589fca006baa5dc46cb523e094f1b565f00ffa
Max Shen <gshen@college.harvard.edu>
2015-08-05 01:12:17 +0000

    implemented editing questions

--- client/templates/question.js
+++ client/templates/question.js
@@ -1,5 +1,5 @@
 Template.question.events({
-  // remove question and attached answers if delete button is clicked
+  // remove question if delete button is clicked
   "click .delete": function () {
     Meteor.call('removeQuestion', this._id); 
   }
@@ -10,6 +10,10 @@ Template.question.helpers({
   // checks whether question is allowed to be deleted
   deletable: function(){
     return (this.userId === Meteor.userId() && Answers.find({questionId: this._id}).count() === 0);
+  },
+  // checks whether question belongs to current user
+  ownPost: function(){
+    return this.userId === Meteor.userId();
   },
   // counts the number of answers on the current question
   commentsCount: function() {   
