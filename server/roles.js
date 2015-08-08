5a30817f7ac1c220e0b8a1f000951949e0154c6b
Max Shen <gshen@college.harvard.edu>
2015-08-08 07:36:04 +0000

    added roles based authorization system to separate harvard and companies. reset DB

---
+++ server/roles.js
@@ -0,0 +1,26 @@
+/** only have to do once, when initiating the website
+Meteor.startup(function(){
+  Roles.createRole('harvard');
+  Roles.createRole('admin');
+  Roles.createRole('company');
+}); */
+
+// set the role of the user when it is created
+Accounts.onCreateUser(function(options, user){
+  user.profile = options.profile;
+  var isHarvard = function(email){
+    var n = email.indexOf("@");
+    var extension = email.substring(n);
+    return extension === "@college.harvard.edu";
+  };   
+  var role;
+  if (isHarvard(options.email))
+    role = 'harvard';
+  else
+    role = 'company';
+   
+  Meteor.setTimeout(function () {
+     Roles.addUsersToRoles(user._id, role);
+  }, 10);
+  return user;
+});
