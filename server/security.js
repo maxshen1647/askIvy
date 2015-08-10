aab19068fa707699600fde316286fa3955bed911
Max Shen <gshen@college.harvard.edu>
2015-08-10 06:49:01 +0000

    implemented security feature: cannot have active sessions of the same account at differnet locations

---
+++ server/security.js
@@ -0,0 +1,11 @@
+
+// make sure the same account cannot be signed in at two different locations 
+Accounts.validateLoginAttempt(function(attempt) {
+  if (!attempt.allowed){
+    return false;
+  } else {    
+    var loginAttemptIp = attempt.connection.clientAddress;
+    var sameUser = UserStatus.connections.find({userId: attempt.user._id, ipAddr: {$ne: loginAttemptIp}}).count();
+    return sameUser === 0;
+  }
+});
