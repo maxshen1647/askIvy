cdb8255162672d43c6496d4dc4b22062bbac7b7c
Max Shen <gshen@college.harvard.edu>
2015-08-13 07:12:10 +0000

    registration codes implemented

--- server/security.js
+++ server/security.js
@@ -1,3 +1,41 @@
+//checks whether given email is from harvard
+isHarvard = function(email){
+    var n = email.indexOf("@");
+    var extension = email.substring(n);
+    return extension === "@college.harvard.edu";
+  };
+
+// validity of registration code
+isValidCode = function(code) {
+           // try to find the code in the db
+           var codeDoc = AccessCodes.findOne({_id: code, valid: true});
+           // if not found, return false; if found, return true and set code to be expired
+           if (!codeDoc)
+             return false;
+           AccessCodes.update(code, {$set: {valid: false}});
+           return true;
+           };
+
+// // checks code and set the role of the user when it is created
+Accounts.onCreateUser(function(options, user){
+  var code = options.profile.accesscode;
+  
+  if (!isHarvard(options.email) && !isValidCode(code)) {
+    throw new Meteor.Error('code', 'Incorrect or expired registration code ');
+  } else {
+    Meteor.setTimeout(function () {
+    if (isHarvard(options.email))
+      Roles.addUsersToRoles(user._id, 'harvard', 'ivy');
+    else
+      Roles.addUsersToRoles(user._id, 'student', 'ivy100');
+    }, 10);
+  }  
+  
+  user.profile = options.profile;
+  return user;
+});
+
+
 // make sure the same account cannot be signed in at two different locations, unless ivy student 
 Accounts.validateLoginAttempt(function(attempt) { 
   if (!attempt.allowed){
@@ -12,4 +50,8 @@ Accounts.validateLoginAttempt(function(attempt) {
 });
 
 AccessCodes = new Mongo.Collection("accessCodes");
+
+AccessCodes.insert({_id: 'a', valid: true});
+AccessCodes.insert({_id: 'b', valid: true});
+AccessCodes.insert({_id: 'c', valid: true});
 
