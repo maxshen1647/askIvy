866ceb4cbd7909fc431966a7ddd493a8aad6776b
Max Shen <gshen@college.harvard.edu>
2015-08-20 23:44:37 +0000

    create profile form now redirects to profile page on success. user is alerted if profile already exists

--- client/templates/profileInsert.js
+++ client/templates/profileInsert.js
@@ -1,5 +1,20 @@
+// automatically feeds current user information to autoform, which automatically includes it when creating the profile
 Template.profileInsert.helpers({
     'userData': function(ownerID) {
         return Meteor.users.findOne();
     }
+});
+
+// alerts user if profile already exists
+// redirects to the profile page on successful form submission
+AutoForm.addHooks(['insertProfile'], {
+  after: {
+    insert: function(error, result) {
+      if (error) {
+        alert('Profile already exists');
+      } else {
+        Router.go('/profile/' + this.docId);
+      }
+    },
+  }
 });
