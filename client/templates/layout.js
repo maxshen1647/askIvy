bc1071097dbb95a4acae1b7e7f686931fccae1ac
Max Shen <gshen@college.harvard.edu>
2015-08-20 22:50:43 +0000

    added profile button to nav bar

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -2,3 +2,9 @@ Template.registerHelper('harvardStudent', function(){
   // checks whether current user is registered with a college.harvard.edu email
   return isHarvardStudent();
 });
+
+Template.layout.helpers({
+  profileUrl: function (){
+    return ('/profile/' + Meteor.userId());
+  }
+});
